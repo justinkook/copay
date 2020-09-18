f170513e5bf85ca27b14c7c91b3f74990a95e91d
Justin Kook <jkook@bitpay.com>
2020-09-18 14:09:15 -0400

    remove importScripts and add script tag to index.html

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -3,8 +3,6 @@
 const CACHE = "pwabuilder-offline";
 const QUEUE_NAME = "bgSyncQueue";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
